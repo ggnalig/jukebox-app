5999b2d72e354e03e99701a626428400d7345dca
GILANG <rahmatgilangmaulana9@gmail.com>
2022-04-11 02:16:05 +0700

    fix(track-reducers, packages): handle error and remove unused package

--- src/reducers/tracksReducer.js
+++ src/reducers/tracksReducer.js
@@ -1,10 +1,11 @@
 
-import {GET_TOP_TRACKS, SEARCH_TRACK} from '../types'
+import {GET_TOP_TRACKS, SEARCH_TRACK, USERS_ERROR} from '../types'
 
 const initialState = {
     tracks: [],
     results: [],
-    loading:true
+    loading: true,
+    error: {}
 }
 
 export const trackReducer = (state = initialState, action) => {
@@ -22,6 +23,13 @@ export const trackReducer = (state = initialState, action) => {
         return {
             ...state,
             results:action.payload,
+            loading:false
+
+        }
+        case USERS_ERROR:
+        return {
+            ...state,
+            error:action.payload,
             loading:false
 
         }
